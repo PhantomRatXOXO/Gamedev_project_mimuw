e89f28c8ee3eaed2e570d8eaf419525979446464
Wojciech-Galach <Wojciech@galach.pl>
2026-01-23 00:18:08 +0100

    Healthbar for boss

--- Source/GamedevProject/Public/Enemies/BossEnemy.h
+++ Source/GamedevProject/Public/Enemies/BossEnemy.h
@@ -4,6 +4,9 @@
 #include "Enemies/EnemyBase.h"
 #include "BossEnemy.generated.h"
 
+class UHealthBarWidget;
+class UUserWidget;
+
 UCLASS()
 class GAMEDEVPROJECT_API ABossEnemy : public AEnemyBase
 {
@@ -11,4 +14,17 @@ class GAMEDEVPROJECT_API ABossEnemy : public AEnemyBase
 
 public:
 	ABossEnemy();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void HandleHealthChanged(float CurrentHealth, float InMaxHealth) override;
+
+	// Widget class for the boss health bar (create a WBP_BossHealthBar in Editor)
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Boss|UI")
+	TSubclassOf<UUserWidget> BossHealthBarClass;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UHealthBarWidget> BossHealthBar = nullptr;
 };
